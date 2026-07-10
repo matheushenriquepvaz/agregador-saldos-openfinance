4d9553bc7c950f28ac38346bb672d3649695c3ff
Matheus Vaz <matheus.vaz@caixa.gov.br>
2026-07-10 18:19:42 -0300

    feature: commit com estrutura inicial

--- src/main/java/ada/teste/config/RedisCacheConfig.java
+++ src/main/java/ada/teste/config/RedisCacheConfig.java
@@ -3,6 +3,7 @@ package ada.teste.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -12,6 +13,7 @@ import org.springframework.data.redis.serializer.RedisSerializationContext.Seria
 import java.time.Duration;
 
 @Configuration
+@ConditionalOnProperty(prefix = "app.cache.redis", name = "enabled", havingValue = "true")
 public class RedisCacheConfig {
 
     @Bean
